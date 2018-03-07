23e2dcc24d63f28f2932125bc7334b17ccbe0210
diriarte <diriarte@condorlabs.io>
2018-03-07 12:42:57 -0500

    initial commit

---
+++ app/src/main/java/com/davidsantiagoiriarte/wizardexample/CardFragmentPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.davidsantiagoiriarte.wizardexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.widget.CardView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardFragmentPagerAdapter extends FragmentStatePagerAdapter implements CardAdapter {
+
+    private List<CardFragment> mFragments;
+    private float mBaseElevation;
+
+    public CardFragmentPagerAdapter(FragmentManager fm, float baseElevation) {
+        super(fm);
+        mFragments = new ArrayList<>();
+        mBaseElevation = baseElevation;
+
+        for(int i = 0; i< 5; i++){
+            if(i%2==0)
+            addCardFragment(new CardFragment());
+            else
+                addCardFragment(new CardFragment2());
+        }
+    }
+
+    @Override
+    public float getBaseElevation() {
+        return mBaseElevation;
+    }
+
+    @Override
+    public CardView getCardViewAt(int position) {
+        return mFragments.get(position).getCardView();
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Object fragment = super.instantiateItem(container, position);
+        mFragments.set(position, (CardFragment) fragment);
+        return fragment;
+    }
+
+    public void addCardFragment(CardFragment fragment) {
+        mFragments.add(fragment);
+    }
+
+}
---
+++ app/src/main/java/com/davidsantiagoiriarte/wizardexample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.davidsantiagoiriarte.wizardexample
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private var mFragmentCardAdapter: CardFragmentPagerAdapter? = null
+
+
+    private var mFragmentCardShadowTransformer: ShadowTransformer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mFragmentCardAdapter = CardFragmentPagerAdapter(supportFragmentManager,
+                dpToPixels(2, this))
+        mFragmentCardShadowTransformer = ShadowTransformer(viewPager, mFragmentCardAdapter)
+        viewPager.adapter = mFragmentCardAdapter
+        viewPager.setPageTransformer(false, mFragmentCardShadowTransformer)
+        viewPager.offscreenPageLimit = 3
+        mFragmentCardShadowTransformer!!.enableScaling(false)
+
+    }
+
+    fun dpToPixels(dp: Int, context: Context): Float {
+        return dp * context.resources.displayMetrics.density
+    }
+}
